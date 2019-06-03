5beee61aa8cd0f312f8b547a5d0f1c5a9e585fb2
Six-s <brenlink@gmail.com>
2019-06-02 23:41:51 -0600

    Begin work on animation for contact me button. Opening animation is complete

--- src/contact.js
+++ src/contact.js
@@ -12,6 +12,48 @@ class contact extends React.Component {
         }
     }
 
+    animation() {
+        //I'm a complete idiot, and I hate myself
+        let nums = [1,2,3,4,5]
+        console.log(this.state.active)
+        if(this.state.active == false){
+            let i = -40
+            nums.map(function(num){
+            console.log('run animation')
+                let animation = anime({
+                    targets: '.Square'+num,
+                    keyframes: [
+                    {translateY: i},
+                    ],
+                    duration: 4000,
+                    easing: 'easeOutElastic(1, .8)',
+                    loop: false
+                });
+                i -= 40
+                animation.play();
+            });
+        } else {
+            console.log('closing animation!')
+            // nums = [5,4,3,2,1]
+            let i = 40
+            nums.map(function(num){
+            console.log('run animation')
+                let animation = anime({
+                    targets: '.Square'+num,
+                    keyframes: [
+                    {translateY: 40},
+                    ],
+                    duration: 4000,
+                    easing: 'easeOutElastic(1, .8)',
+                    loop: false
+                });
+                // i += 40
+                animation.play();
+            });
+        }
+        console.log('animation done')
+    }
+
     buttonClick(){
         console.log('button clicked!')
         if(this.state.active == false){
@@ -20,18 +62,27 @@ class contact extends React.Component {
                 name: 'contactsButtonClicked',
                 text: 'Close'
             });
+            this.animation()
         } else {
             this.setState({
                 active: false,
                 name: 'contactsButton',
                 text: 'Contact Me'
             })
+            this.animation()
+
         }
     }
     
     render () {
+
         return(
             <div className='contactsContainer'>
+                <div className='Square Square1'></div>
+                <div className='Square Square2'></div>
+                <div className='Square Square3'></div>
+                <div className='Square Square4'></div>
+                <div className='Square Square5'></div>
                 <button 
                 className={this.state.name}
                 onClick={this.buttonClick.bind(this)}>
