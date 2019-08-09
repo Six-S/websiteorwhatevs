9117b6a65d36f42b19c91ded79d328032ebf6bfd
Six-s <brenlink@gmail.com>
2019-08-09 01:07:24 -0600

    Okay for real this time fix the animation for good

--- src/splashScreen.js
+++ src/splashScreen.js
@@ -3,7 +3,7 @@ import anime from 'animejs';
 
 class splashscreen extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             labelState: 'label'
@@ -11,35 +11,40 @@ class splashscreen extends React.Component {
         console.log('props?', props)
     }
 
-    enterClicked(){
+    enterClicked() {
         console.log('enter was clicked....');
-        this.setState({labelState: 'labelClicked'});
+        this.setState({ labelState: 'labelClicked' });
         this.props.enterClicked();
         let nums = [0, 1, 2]
         let transY = [-250, 0, 250]
-        nums.map(function(num){
-            let animation = anime({
-                targets: '.line'+num,
-                keyframes: [
-                    {translateY: transY[num]},
-                    {scaleX: '10'}
-                  ],
-                easing: 'easeOutExpo',
-                duration: 1000
+        nums.map(function (num) {
+            let animation = anime.timeline({
+                easing: 'linear',
+                duration: 2000
             });
+            animation.add({
+                targets: '.line' + num,
+                translateY: transY[num],
+                duration: 1000,
+            });
+            animation.add({
+                targets: '.line' + num,
+                duration: 1000,
+                width: '80vw',
+            },1000);
             animation.play();
         });
     }
 
     render() {
         return (
-            <div className="content" style={{textAlign: 'center'}}>
+            <div className="content" style={{ textAlign: 'center' }}>
                 <div className='line line0'></div>
                 <div className='line line1'></div>
                 <div className='line line2'></div>
                 <label
-                className={this.state.labelState}
-                onClick={this.enterClicked.bind(this)}>
+                    className={this.state.labelState}
+                    onClick={this.enterClicked.bind(this)}>
                     Enter
                 </label>
             </div>
